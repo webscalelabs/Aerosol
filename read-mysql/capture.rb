bf7fa64ad60db8d8df824f442e319c085cc10ffd
webscalelabs <webscalelabs10@illuminatus.org>
2011-04-20 03:20:14 -0700

    works for now, invalid sql, but useful information

---
+++ read-mysql/capture.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'ackptoo123')
+
+query = false
+build = ""
+table = ""
+ARGF.read.split("\n").each do |line|
+  if line =~ /^###/
+    query = true
+    sql = line.gsub(/^###[ ]+(.*)/, '\1')
+    /^(DELETE FROM|INSERT INTO|UPDATE) ([^ ]+)/.match(sql)
+    rm = Regexp.last_match(2)
+    table = rm unless rm == nil
+
+    build+=sql + ' '
+  else
+    if query == true
+
+      cols = client.query("SHOW COLUMNS FROM #{table}", :symbolize_keys => true )
+      col_names = Array.new
+      cols.each do |col|
+        col_names << col[:Field]
+      end
+      build = build.gsub(/@(\d+)/) do |s|
+        n = s[1..s.size].to_i - 1
+        col_names[n]
+      end
+
+      puts build
+      build = ""
+      table = ""
+    end
+    query = false
+  end
+end
